6182da5092a06fe9c74a3294636644bd83252267
Kristin Barr <kristinbarr@gmail.com>
2019-08-23 12:38:45 -0400

    removed unused code

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -7,7 +7,6 @@ const Login = () => {
     password: ''
   })
 
-  console.log('creds:', credentials)
   const handleChange = (e) => {
     setCredentials({
       ...credentials,
@@ -22,7 +21,7 @@ const Login = () => {
       .then((res) => {
         localStorage.setItem('token', res.data.payload)
       })
-      .catch((err) => console.log('unable to login!',err.response))
+      .catch((err) => console.log('Unable to login!', err.response))
   }
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
